57716f7ba049fd89a4fc7bfd95e89f84d198ebda
jackjakarta <al.termure@gmail.com>
2024-01-11 15:52:48 +0100

    updated test command to take arguments

--- website/management/commands/say_hi.py
+++ website/management/commands/say_hi.py
@@ -4,5 +4,16 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = "Test command"
 
+    def add_arguments(self, parser):
+        # Define command line arguments here
+        parser.add_argument('arg1', type=str, help='First argument')
+        # parser.add_argument('arg2', type=int, help='Second argument')
+
     def handle(self, *args, **options):
-        print("Hello World!")
+        # Access the arguments using options dictionary
+        arg1 = options['arg1']
+        # arg2 = options['arg2']
+
+        # Your command logic here
+        print(f"Argument 1: {arg1}")
+        # print(f"Argument 2: {arg2}")
