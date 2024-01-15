d07470cabed9b3ffbf1db6801100f39ce0305721
jackjakarta <al.termure@gmail.com>
2024-01-15 02:52:59 +0100

    tweaked email env settings

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -182,7 +182,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_USE_SSL = config('EMAIL_USE_SSL')
+EMAIL_USE_SSL = config('EMAIL_SECURE')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
