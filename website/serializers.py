150c8593adbf58bf21f76a9dcab7bdb9ead49cf5
jackjakarta <al.termure@gmail.com>
2024-01-17 13:45:52 +0100

    tweaked api serializers for depth and field exclusion

--- website/serializers.py
+++ website/serializers.py
@@ -1,21 +1,47 @@
 from rest_framework import serializers
 from .models import Event, Venue, Promoter
+from django.contrib.auth import get_user_model
+
+AuthUser = get_user_model()
 
 
-class EventSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Event
+        model = AuthUser
+        exclude = [
+            "last_login",
+            "password",
+            "is_superuser",
+            "is_staff",
+            "groups",
+            "user_permissions",
+        ]
+
+
+class VenueSerializer(serializers.ModelSerializer):
+    manager = UserSerializer()
+
+    class Meta:
+        model = Venue
         exclude = ["created_at", "updated_at", ]
         depth = 1
 
 
-class VenueSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Venue
-        exclude = ["created_at", "updated_at", ]
-
-
 class PromoterSerializer(serializers.ModelSerializer):
+    manager = UserSerializer()
+
     class Meta:
         model = Promoter
         exclude = ["created_at", "updated_at", ]
+        depth = 1
+
+
+class EventSerializer(serializers.ModelSerializer):
+    venue = VenueSerializer()
+    promoter = PromoterSerializer()
+    manager = UserSerializer()
+
+    class Meta:
+        model = Event
+        exclude = []
+        depth = 1
