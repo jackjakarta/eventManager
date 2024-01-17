92f5129abcf2ec97cac5058e14ca51d037dd31d7
jackjakarta <al.termure@gmail.com>
2024-01-17 12:30:57 +0100

    added carousel and search bar to home page, added depth parameter to events serializer to show venue info

--- website/serializers.py
+++ website/serializers.py
@@ -6,6 +6,7 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         exclude = ["created_at", "updated_at", ]
+        depth = 1
 
 
 class VenueSerializer(serializers.ModelSerializer):
--- website/views/static.py
+++ website/views/static.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from website.models import Event
 
 
 # Static Pages Views
 def home(request):
-    return render(request, "website/home.html", {})
+    events_qs = Event.objects.all().order_by("event_date")[:5]
+    return render(request, "website/home.html", {
+        "events": events_qs,
+    })
 
 
 def app_docs(request):
