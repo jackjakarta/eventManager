a9d16f8df937b32487cc9597e3fb2baa4e6bee99
jackjakarta <al.termure@gmail.com>
2024-01-18 05:26:18 +0100

    changed all buttons to small

--- event_manager/urls.py
+++ event_manager/urls.py
@@ -9,18 +9,24 @@ import website.viewsets as wb_views
 import social.viewsets as scl_views
 
 router = routers.DefaultRouter()
+
+# Events, Venues, Artists, Promoters API Routes
 router.register(r'events', wb_views.EventsViewSet, 'events')
 router.register(r'venues', wb_views.VenuesViewSet, 'venues')
 router.register(r'promoters', wb_views.PromotersViewSet, 'promoters')
+
+# Social App API Routes
 router.register(r'posts', scl_views.PostsViewSet, 'posts')
 router.register(r'post-comments', scl_views.PostCommentsViewSet, 'post-comments')
 
 
 urlpatterns = [
+    # Apps Urls
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
     path('social/', include('social.urls')),
 
+    # API Urls
     path('api/', include(router.urls)),
     path('api/auth/', jwt_views.TokenObtainPairView.as_view(), name='api_auth'),
     path('api/auth/refresh/', jwt_views.TokenRefreshView.as_view(), name='api_auth_refresh'),
