dd8b696dfe21ffb086dbdaa29cdc9d7d9aa5c35f
jackjakarta <al.termure@gmail.com>
2024-02-13 16:16:35 +0100

    implemented generate api key on user profile

--- website/urls/user_auth.py
+++ website/urls/user_auth.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from website.views.auth import login_user, logout_user, register_user
+from website.views.auth import login_user, logout_user, register_user, generate_api_key
 
 
 app_name = 'user_auth'
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('register/', register_user, name='register'),
+    path('generate-api-key/', generate_api_key, name='generate_api_key'),
 ]
