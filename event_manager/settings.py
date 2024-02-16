df3c45b1ed25acd2f685b8267690772ed84984d0
jackjakarta <al.termure@gmail.com>
2024-02-16 22:38:15 +0100

    minor settings.py update

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -30,10 +30,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY', default=secrets.token_urlsafe())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-if DEBUG:
-    ALLOWED_HOSTS = []
-else:
-    ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
