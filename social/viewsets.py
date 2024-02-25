84d32e57bd3ac3de948e0de8f75e2aed0b6ca890
jackjakarta <al.termure@gmail.com>
2024-02-25 04:44:27 +0100

    fixed User API Key permission class

--- social/viewsets.py
+++ social/viewsets.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_api_key.permissions import HasAPIKey
-from users.utils.apikey_auth import CustomAPIKeyAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from users.utils.apikey_auth import UserHasAPIKey
 from .models import Post, PostComment
 from .serializers import PostSerializer, PostCommentSerializer
 
@@ -11,12 +11,12 @@ from .serializers import PostSerializer, PostCommentSerializer
 class PostsViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [HasAPIKey | IsAuthenticatedOrReadOnly | CustomAPIKeyAuthentication]
+    permission_classes = [HasAPIKey | UserHasAPIKey | IsAuthenticated]
     authentication_classes = (JWTAuthentication, )
 
 
 class PostCommentsViewSet(viewsets.ModelViewSet):
     queryset = PostComment.objects.all()
     serializer_class = PostCommentSerializer
-    permission_classes = [HasAPIKey | IsAuthenticated | CustomAPIKeyAuthentication]
+    permission_classes = [HasAPIKey | UserHasAPIKey | IsAuthenticated]
     authentication_classes = (JWTAuthentication, )
--- website/viewsets.py
+++ website/viewsets.py
@@ -2,34 +2,35 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework_api_key.permissions import HasAPIKey
 from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from users.utils.apikey_auth import UserHasAPIKey
 from .models import Event, Venue, Promoter, Artist
 from .serializers import EventSerializer, VenueSerializer, PromoterSerializer, ArtistSerializer
-from users.utils.apikey_auth import CustomAPIKeyAuthentication
 
 
 class ArtistsViewSet(viewsets.ModelViewSet):
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
-    permission_classes = [HasAPIKey | IsAuthenticatedOrReadOnly | CustomAPIKeyAuthentication]
+    permission_classes = [HasAPIKey | UserHasAPIKey | IsAuthenticatedOrReadOnly]
     authentication_classes = (JWTAuthentication, )
 
 
 class EventsViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    permission_classes = [HasAPIKey | IsAuthenticated | CustomAPIKeyAuthentication]
+    permission_classes = [HasAPIKey | UserHasAPIKey | IsAuthenticated]
     authentication_classes = (JWTAuthentication, )
 
 
 class VenuesViewSet(viewsets.ModelViewSet):
     queryset = Venue.objects.all()
     serializer_class = VenueSerializer
-    permission_classes = [HasAPIKey | IsAuthenticated | CustomAPIKeyAuthentication]
+    permission_classes = [HasAPIKey | UserHasAPIKey | IsAuthenticated]
     authentication_classes = (JWTAuthentication, )
 
 
 class PromotersViewSet(viewsets.ModelViewSet):
     queryset = Promoter.objects.all()
     serializer_class = PromoterSerializer
-    permission_classes = [HasAPIKey | IsAuthenticated | CustomAPIKeyAuthentication]
+    permission_classes = [HasAPIKey | UserHasAPIKey | IsAuthenticated]
     authentication_classes = (JWTAuthentication, )
