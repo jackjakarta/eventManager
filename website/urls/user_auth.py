791f417835339c1b3cabff2bc3b14b955362838b
jackjakarta <al.termure@gmail.com>
2024-03-01 02:58:20 +0100

    implemented social auth with goolge and facebook

--- website/urls/user_auth.py
+++ website/urls/user_auth.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from website.views.auth import login_user, logout_user, register_user, generate_api_key
+from django.urls import path, include
 
+from website.views.auth import login_user, logout_user, register_user, generate_api_key
 
 app_name = 'user_auth'
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('register/', register_user, name='register'),
+    path('social-auth/', include('social_django.urls')),
     path('generate-api-key/', generate_api_key, name='generate_api_key'),
 ]
--- website/utils/email.py
+++ website/utils/email.py
@@ -20,3 +20,24 @@ def send_register_newsletter_email(sub_email):
     )
     mail.content_subtype = "html"
     mail.send()
+
+
+# Sends an email when a user subscribes to the newsletter
+def send_register_user_email(first_name, last_name, to_email):
+    context = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": to_email,
+    }
+
+    template = get_template("website/email/register_user_email.html")
+    content = template.render(context)
+
+    mail = EmailMultiAlternatives(
+        subject="Thank you for registering to our app!",
+        body=content,
+        from_email=settings.EMAIL_HOST_USER,
+        to=[to_email],
+    )
+    mail.content_subtype = "html"
+    mail.send()
